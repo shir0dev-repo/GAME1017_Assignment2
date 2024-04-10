a366867bd8dafd72571b9b93912b1b10a781812c
James Mitchell <v010000010101001101010011@gmail.com>
2024-04-10 02:58:36 -0400

    added exceptions to sound manager

--- Assets/!Scripts/Managers/SoundManager.cs
+++ Assets/!Scripts/Managers/SoundManager.cs
@@ -1,5 +1,5 @@
 
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -34,23 +34,36 @@ public class SoundManager : Singleton<SoundManager>
     _musicSource.loop = true;
     _musicSource.playOnAwake = true;
     SetMusicVolume(_musicVolumeCache);
-
+    PlayMusicNoFade("Intro");
   }
 
   private void InitResourceDictionaries()
   {
-
+    _musicDictionary.Add("Intro", Resources.Load<AudioClip>("Audio/Intro"));
   }
 
   public void PlaySound(string soundKey)
   {
+    if (!_sfxDictionary.ContainsKey(soundKey))
+      throw new System.IndexOutOfRangeException(soundKey + " was not present in the sound effect dictionary!");
+
     _sfxSource.PlayOneShot(_sfxDictionary[soundKey]);
   }
 
   public void PlayMusic(string soundKey)
   {
+    if (!_musicDictionary.ContainsKey(soundKey))
+      throw new System.IndexOutOfRangeException(soundKey + " was not present in the music dictionary!");
+
     if (_currentFadeInOutCO != null) return;
-    _currentFadeInOutCO = StartCoroutine(MusicFadeCoroutine());
+    _currentFadeInOutCO = StartCoroutine(MusicFadeCoroutine(_musicDictionary[soundKey]));
+  }
+
+  public void PlayMusicNoFade(string soundKey)
+  {
+    _musicSource.Stop();
+    _musicSource.clip = _musicDictionary[soundKey];
+    _musicSource.Play();
   }
 
   public void SetSFXVolume(float value)
@@ -72,14 +85,25 @@ public class SoundManager : Singleton<SoundManager>
     SetMusicVolume(_musicVolumeCache);
   }
 
-  private IEnumerator MusicFadeCoroutine()
+  private IEnumerator MusicFadeCoroutine(AudioClip clip)
   {
     float timeElapsed = 0;
-    float fadeDuration = 3f;
-    while (timeElapsed < fadeDuration)
+    float halfFadeDuration = 2.5f;
+    while (timeElapsed < halfFadeDuration)
     {
       timeElapsed += Time.deltaTime;
-      _musicSource.volume *= 1 - (timeElapsed / fadeDuration);
+      _musicSource.volume *= 1 - (timeElapsed / halfFadeDuration);
+      yield return new WaitForEndOfFrame();
+    }
+
+    timeElapsed = 0;
+    _musicSource.clip = clip;
+
+    while (timeElapsed < halfFadeDuration)
+    {
+      timeElapsed += Time.deltaTime;
+      _musicSource.volume = _masterVolumeCache * (timeElapsed / halfFadeDuration);
+      yield return new WaitForEndOfFrame();
     }
   }
 }
